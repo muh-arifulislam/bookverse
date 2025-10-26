b4e863bbc8eae892e07c195dc4cc70ebb8f23bdc
muh-arifulislam <arifibnenam@gmail.com>
2025-10-26 23:51:56 +0600

    form added to contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -14,25 +14,54 @@ export default function Contacts() {
             </ul>
           </div>
         </div>
-        <div className="grid grid-cols-2 gap-8">
+        <div className="grid grid-cols-2 gap-8 my-20">
           <div>
-            <h2>Get in Touch</h2>
+            <h2 className="text-2xl font-semibold mb-6">Get in Touch</h2>
             <p>
               Duis aute irure dolor in reprehenderit in voluptate velit esse
               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
               cupidatat non proident, sunt in culpa qui officia deserunt mollit
               anim id est laborum.
             </p>
-            <div>
+            <div className="mt-6">
               <p>+1650-243-0000</p>
               <p>info@yourcompany.com</p>
               <p>North Melbourne VIC 3051, Australia</p>
             </div>
           </div>
           <div>
-            <h2>Send A Message</h2>
-            <div>
-              <form action=""></form>
+            <h2 className="text-2xl font-semibold mb-6">Send A Message</h2>
+            <div className="">
+              <form action="">
+                <div className="flex items-center gap-6 mb-6">
+                  <div className="w-full max-w-xs">
+                    <input
+                      className="border-[1px] px-2 py-1 rounded border-slate-400/30 outline-none w-full bg-white"
+                      type="text"
+                      placeholder="Name"
+                    />
+                  </div>
+                  <div className="w-full max-w-xs">
+                    <input
+                      className="border-[1px] px-2 py-1 rounded border-slate-400/30 outline-none w-full bg-white"
+                      type="text"
+                      placeholder="Email"
+                    />
+                  </div>
+                </div>
+                <div className="w-full max-w-lg">
+                  <textarea
+                    className="border-[1px] px-2 py-1 rounded border-slate-400/30 outline-none w-full bg-white"
+                    placeholder="Message"
+                    rows={6}
+                  />
+                </div>
+                <div className="my-6">
+                  <button className="py-4 bg-red-500 w-full font-bold uppercase text-lg rounded">
+                    Submit
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
